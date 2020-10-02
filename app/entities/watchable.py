f8b6ef9c510a9705cbe614d4cf1546bc9a60f63f
Alex Menor <alex4menor@gmail.com>
2020-10-02 20:04:59 +0200

    adds docstring to watchable module

--- app/entities/watchable.py
+++ app/entities/watchable.py
@@ -1,7 +1,10 @@
+""" This module contains watchable entity """
+
 from dataclasses import dataclass
 
 @dataclass
 class Watchable:
+    """ Represents something you can watch in a streaming platform """
     title: str
     synopsis: str
     year: int
