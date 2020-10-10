9cb225b45f7b77885c331855da96689eac6d73ca
Alex Menor <alex4menor@gmail.com>
2020-10-10 11:49:40 +0200

    Añade tests para add_user method en session. Avanza #21.

--- app/tests/test_session.py
+++ app/tests/test_session.py
@@ -1,5 +1,5 @@
 from ..entities.user import User
-from ..entities.session import Session
+from ..entities.session import Session, NotMoreUsersAllowedException
 from ..entities.watchable import Watchable
 
 import pytest
@@ -18,3 +18,24 @@ def test_users_getter(user, watchables):
     session = Session("OtroID", user, watchables)
     assert session.get_users()[0].id == user.id 
 
+def test_add_user(user, watchables):
+    session = Session("OtroID", user, watchables)
+    new_user = User("Nuevo_user")
+
+    session.add_user(new_user)
+
+    users = session.get_users()
+
+    assert len(users) == 2
+    assert users[0].id == user.id
+    assert users[1].id == new_user.id
+
+def test_add_user_throws_exception(user, watchables):
+    session = Session("OtroID", user, watchables)
+    new_user = User("Nuevo_user")
+
+    for i in range(0, Session.MAX_USERS_PER_SESSION - 1):
+        session.add_user(new_user)
+
+    with pytest.raises(NotMoreUsersAllowedException):
+        session.add_user(new_user)
