97dc9f4c31c1814403fe6d084362afb996a56da4
Alex Menor <alex4menor@gmail.com>
2020-10-10 10:07:15 +0200

    Define la clase session. Avanza #14 porque representa el grupo. Avanza #20 porque añadiré métodos en esta clase que se testearan.

---
+++ app/entities/session.py
@@ -0,0 +1,13 @@
+""" Defines session class """
+
+from dataclasses import dataclass
+from typing import List
+from .user import User
+from .watchable import Watchable
+
+@dataclass
+class Session:
+  """ Contains users, the watchables they are choosing from and their choices """
+  id: str
+  users: List[User]
+  watchables: List[Watchable]
