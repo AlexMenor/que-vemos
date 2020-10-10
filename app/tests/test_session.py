45e920d560e2c86c365c5b0bbd7579cadf11f820
Alex Menor <alex4menor@gmail.com>
2020-10-10 11:43:04 +0200

    fixes linting errors

--- app/tests/test_session.py
+++ app/tests/test_session.py
@@ -6,15 +6,15 @@ import pytest
 
 @pytest.fixture
 def user():
-  return User("UnID")
+    return User("UnID")
 
 @pytest.fixture
 def watchables():
-  watchable = Watchable("Narcos", "Se centra en la historia real de una peligrosa difusión y propagación de una red de cocaína por todo el mundo durante los años 70 y 80.", 2015)
-  return [watchable] 
+    watchable = Watchable("Narcos", "Se centra en la historia real de una peligrosa difusión y propagación de una red de cocaína por todo el mundo durante los años 70 y 80.", 2015)
+    return [watchable] 
 
 
 def test_users_getter(user, watchables):
-  session = Session("OtroID", user, watchables)
-  assert session.get_users()[0].id == user.id 
+    session = Session("OtroID", user, watchables)
+    assert session.get_users()[0].id == user.id 
 
