7c8da6e53252b3981452ed2491780565ebe6422e
Alex Menor <alex4menor@gmail.com>
2020-10-22 16:25:06 +0200

    removes test script

--- prueba.sh
+++
@@ -1,6 +0,0 @@
-git diff --name-only HEAD^ HEAD | grep -E "(Dockerfile|pyproject.toml)"
-  if [[ $? ]]; then
-	      echo "::set-output name=run_docker_build::true"
-		    else
-			        echo "::set-output name=run_docker_build::false"
-				  fi
