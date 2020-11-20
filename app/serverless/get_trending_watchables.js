69086dbe598b31e10511c7abb2af296aae6825fd
Alex Menor <alex4menor@gmail.com>
2020-11-20 10:02:43 +0100

    añade ordenación en get_trending_watchables para avanzar #49 y #53

--- app/serverless/get_trending_watchables.js
+++ app/serverless/get_trending_watchables.js
@@ -3,13 +3,23 @@ const watchables = require('./serialized_data.json')
 exports.handler = async function(event, context) {
     let result = watchables;
 
-    const {type, search} = event.queryStringParameters
+    const {type, search, orderBy} = event.queryStringParameters
 
     if (type == 'MOVIE' || type == 'SERIES')
         result = result.filter(watchable => watchable.type == type)
 
     if (search)
         result = result.filter(({title, synopsis}) => title.includes(search) || synopsis.includes(search))
+
+    if (orderBy){
+        const [field, order] = orderBy.split(',')
+
+        if (order == 'ASC')
+            result.sort((a,b) => a[field] - b[field])
+        else
+            result.sort((a,b) => b[field] - a[field])
+
+    }
 
     return {
         statusCode: 200,
