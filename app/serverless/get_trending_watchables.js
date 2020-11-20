ee6f752732c6f9595f8f2853acdc756e19c7489d
Alex Menor <alex4menor@gmail.com>
2020-11-20 09:21:36 +0100

    prueba netlify functions

--- app/serverless/get_trending_watchables.js
+++ app/serverless/get_trending_watchables.js
@@ -1,6 +1,8 @@
+const watchables = require('./serialized_data.json')
+
 exports.handler = async function(event, context) {
 return {
     statusCode: 200,
-    body: JSON.stringify({message: "Hello World"})
+    body: JSON.stringify(watchables)
 };
 }
