d10e00968215b7339324aaed03ed35f5029f2f95
Alex Menor <alex4menor@gmail.com>
2020-11-20 09:38:34 +0100

    añade filtrado por tipo y búsqueda a get_trending_watchables function para avanzar #53 y #49

--- app/serverless/get_trending_watchables.js
+++ app/serverless/get_trending_watchables.js
@@ -1,8 +1,18 @@
 const watchables = require('./serialized_data.json')
 
 exports.handler = async function(event, context) {
-return {
-    statusCode: 200,
-    body: JSON.stringify(watchables)
-};
+    let result = watchables;
+
+    const {type, search} = event.queryStringParameters
+
+    if (type == 'MOVIE' || type == 'SERIES')
+        result = result.filter(watchable => watchable.type == type)
+
+    if (search)
+        result = result.filter(({name, synopsis}) => name.includes(search) || synopsis.includes(search))
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(result)
+    };
 }
