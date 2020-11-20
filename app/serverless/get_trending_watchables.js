7cc1f31f32d20645d6848771a3d9f7b71941cbe3
Alex Menor <alex4menor@gmail.com>
2020-11-20 10:51:44 +0100

    añade cors en netlify function

--- app/serverless/get_trending_watchables.js
+++ app/serverless/get_trending_watchables.js
@@ -1,6 +1,21 @@
 const watchables = require('./serialized_data.json')
 
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+};
+
 exports.handler = async function(event, context) {
+
+    if (event.httpMethod !== 'GET') {
+        // To enable CORS
+        return {
+            statusCode: 200,
+            headers,
+            body: 'This was not a GET request!'
+        };
+    }
     let result = watchables;
 
     const {type, search, orderBy} = event.queryStringParameters
