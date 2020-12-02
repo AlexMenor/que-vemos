3dd8769c9ef96eb982162a508ac5f5b3e5bde5cc
Alex Menor <alex4menor@gmail.com>
2020-12-02 14:13:05 +0100

    Create InMemorySessionStore that implements SessionStore interface to be used within SessionHandler. Advances #62

---
+++ app/data/in_memory_session_store.py
@@ -0,0 +1,20 @@
+from .session_store import SessionStore
+from ..entities.session import Session
+from ..session_handler import SessionNotFound
+
+
+class InMemorySessionStore(SessionStore):
+    def __init__(self):
+        self.__sessions = {}
+
+    async def save(self, session: Session) -> None:
+        # If session already exists in the dict
+        # it makes no difference to assign the same
+        # reference again
+        self.__sessions[session.id] = session
+
+    async def get_one(self, session_id: str) -> Session:
+        if session_id not in self.__sessions:
+            raise SessionNotFound
+
+        return self.__sessions[session_id]
