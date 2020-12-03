7e6714e7a83778d6ab37deaa1722396c9dcfa4c1
Alex Menor <alex4menor@gmail.com>
2020-12-03 18:37:33 +0100

    Add route that allows an user to create a session. Advances #14 y #59

---
+++ app/app.py
@@ -0,0 +1,25 @@
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+from starlette.routing import Route
+
+from.session_handler import SessionHandler
+from .data.watchables_store.in_memory_watchables_store import InMemoryWatchablesStore
+from .data.watchables_store.watchables_store import WatchablesStore
+from .data.session_store.in_memory_session_store import InMemorySessionStore
+from .data.session_store.session_store import SessionStore
+
+watchables_store: WatchablesStore = InMemoryWatchablesStore()
+
+session_store: SessionStore = InMemorySessionStore()
+
+session_handler = SessionHandler(watchables_store, session_store)
+
+
+async def create_session(_):
+    session_id = await session_handler.init_session()
+    return JSONResponse({'session_id': session_id})
+
+
+app = Starlette(debug=True, routes=[
+    Route('/session', create_session, methods=['POST']),
+])
