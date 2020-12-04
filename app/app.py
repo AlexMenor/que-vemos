232bc7883bf90946c4d702d7cfe98ef3a2d88503
Alex Menor <alex4menor@gmail.com>
2020-12-04 10:41:23 +0100

    Añade ruta para que un usuario se pueda unir a una sesión siguiendo la HU11. Avanza #66

--- app/app.py
+++ app/app.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
-from.session_handler import SessionHandler
+from .session_handler import SessionHandler
 from .data.watchables_store.in_memory_watchables_store import InMemoryWatchablesStore
 from .data.watchables_store.watchables_store import WatchablesStore
 from .data.session_store.in_memory_session_store import InMemorySessionStore
 from .data.session_store.session_store import SessionStore
+from .session_handler import SessionNotFound
 
 watchables_store: WatchablesStore = InMemoryWatchablesStore()
 
@@ -12,10 +13,18 @@ session_store: SessionStore = InMemorySessionStore()
 
 session_handler = SessionHandler(watchables_store, session_store)
 
-
 app = FastAPI()
 
 @app.post("/session")
 async def create_session():
     session_id = await session_handler.init_session()
     return {"session_id": session_id}
+
+
+@app.post("/session/{session_id}/user", responses={404: {'description': 'Session not found'}})
+async def user_joins_session(session_id: str):
+    try:
+        return await session_handler.join_user_to_session(session_id)
+    except SessionNotFound:
+        raise HTTPException(status_code=404, detail=f'A session with id {session_id} could not be found')
+
