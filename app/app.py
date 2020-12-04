a28f6a81a65e8f5eb99e7dabf77d618750208b8a
Alex Menor <alex4menor@gmail.com>
2020-12-04 11:20:48 +0100

    Set 201 status code in the post requests and set response model if there is any

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 
+from .entities.user_payload import UserPayload
 from .session_handler import SessionHandler
 from .data.watchables_store.in_memory_watchables_store import InMemoryWatchablesStore
 from .data.watchables_store.watchables_store import WatchablesStore
@@ -15,13 +16,13 @@ session_handler = SessionHandler(watchables_store, session_store)
 
 app = FastAPI()
 
-@app.post("/session")
+@app.post("/session", status_code=201)
 async def create_session():
     session_id = await session_handler.init_session()
     return {"session_id": session_id}
 
 
-@app.post("/session/{session_id}/user", responses={404: {'description': 'Session not found'}})
+@app.post("/session/{session_id}/user", responses={404: {'description': 'Session not found'}}, status_code=201, response_model=UserPayload)
 async def user_joins_session(session_id: str):
     try:
         return await session_handler.join_user_to_session(session_id)
