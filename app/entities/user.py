9d96cd5371d89069d67c7acaecb2b7cb1c52060a
Alex Menor <alex4menor@gmail.com>
2020-12-04 12:50:17 +0100

    Add name field in User to advance #67

--- app/entities/user.py
+++ app/entities/user.py
@@ -1,8 +1,10 @@
 """ Contains user entity """
 
 from dataclasses import dataclass
+from typing import Optional
 
 @dataclass
 class User:
     """ An user represents a client of the service that has joined / started a session """
     id: str
+    name: Optional[str] = None
