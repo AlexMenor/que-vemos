9a3b8668fdf9ad0dec652fba3e43161cbfb2db01
Alex Menor <alex4menor@gmail.com>
2020-12-07 10:51:50 +0100

    Reduce repetition in test_session_routes through fixtures

--- app/tests/integration/test_session_routes.py
+++ app/tests/integration/test_session_routes.py
@@ -1,5 +1,6 @@
 from uuid import UUID
 from fastapi.testclient import TestClient
+import pytest
 
 from app.app import app
 from app.routes.session_routes import SessionHandlerDependency, session_handler_dependency
@@ -19,6 +20,46 @@ class CallableMock:
 
     def __call__(self):
         return self.__mock
+
+
+@pytest.fixture
+def existing_session():
+    watchable = Watchable("Narcos", "Se centra en la historia real de una peligrosa difusión y propagación de una red de cocaína por todo el mundo durante los años 70 y 80.", 2015, WatchableType.SERIES)
+    session = Session('sí-existo', [watchable])
+
+    class Mock(SessionStore):
+        async def save(self, session: Session) -> None:
+            pass
+
+        async def get_one(self, _):
+            return session
+
+    watchables_store = InMemoryWatchablesStore()
+
+    session_handler_mocked = SessionHandlerDependency(watchables_store, Mock())
+
+    app.dependency_overrides[session_handler_dependency] = session_handler_mocked
+
+    yield session
+
+    app.dependency_overrides = {}
+
+@pytest.fixture
+def existing_session_and_user(existing_session):
+    existing_user = User('yo-también')
+    existing_session.add_user(existing_user)
+
+    yield existing_session, existing_user
+
+
+@pytest.fixture
+def existing_session_and_vote(existing_session_and_user):
+    session = existing_session_and_user[0]
+    user = existing_session_and_user[1]
+
+    session.vote(user.id, 0, True)
+
+    yield session
 
 
 def test_create_session():
@@ -52,24 +93,9 @@ def test_joining_with_max_users():
     app.dependency_overrides = {}
 
 
-def test_joining_session():
-    watchable = Watchable("Narcos", "Se centra en la historia real de una peligrosa difusión y propagación de una red de cocaína por todo el mundo durante los años 70 y 80.", 2015, WatchableType.SERIES)
-    session = Session('sí-existo', [watchable])
+def test_joining_session(existing_session):
 
-    class Mock(SessionStore):
-        async def save(self, session: Session) -> None:
-            pass
-
-        async def get_one(self, _):
-            return session
-
-    watchables_store = InMemoryWatchablesStore()
-
-    session_handler_mocked = SessionHandlerDependency(watchables_store, Mock())
-
-    app.dependency_overrides[session_handler_dependency] = session_handler_mocked
-
-    response = client.post('/session/sí-existo/user')
+    response = client.post(f'/session/{existing_session.id}/user')
 
     body = response.json()
 
@@ -79,11 +105,9 @@ def test_joining_session():
 
     UUID(body['user_id'])
 
-    assert body['session_id'] == session.id
+    assert body['session_id'] == existing_session.id
 
     assert 'watchables' in body
-
-    app.dependency_overrides = {}
 
 def test_voting_without_body():
     response = client.post('/session/no-existo/user/yo-tampoco/vote')
@@ -97,84 +121,31 @@ def test_voting_in_non_existing_session():
     assert response.status_code == 404
 
 
-def test_voting_with_non_existing_user():
-    watchable = Watchable("Narcos", "Se centra en la historia real de una peligrosa difusión y propagación de una red de cocaína por todo el mundo durante los años 70 y 80.", 2015, WatchableType.SERIES)
-    session = Session('yo-sí-existo', [watchable])
-
-    class Mock(SessionStore):
-        async def save(self, session: Session) -> None:
-            pass
-
-        async def get_one(self, _):
-            return session
-
-    watchables_store = InMemoryWatchablesStore()
-
-    session_handler_mocked = SessionHandlerDependency(watchables_store, Mock())
-
-    app.dependency_overrides[session_handler_dependency] = session_handler_mocked
-
-    response = client.post('/session/yo-sí-existo/user/yo-no/vote', json={'watchable_index': 0, 'content': "true"})
+def test_voting_with_non_existing_user(existing_session):
+    response = client.post(f'/session/{existing_session.id}/user/yo-no/vote', json={'watchable_index': 0, 'content': "true"})
 
     assert response.status_code == 404
 
-    assert response.json()['detail'] == 'An user with id yo-no could not be found in session yo-sí-existo'
-
-    app.dependency_overrides = {}
+    assert response.json()['detail'] == f'An user with id yo-no could not be found in session {existing_session.id}'
 
 
-def test_voting_non_existing_watchable():
-    watchable = Watchable("Narcos", "Se centra en la historia real de una peligrosa difusión y propagación de una red de cocaína por todo el mundo durante los años 70 y 80.", 2015, WatchableType.SERIES)
-    session = Session('yo-sí-existo', [watchable])
-    session.add_user(User('yo-también'))
-
-    class Mock(SessionStore):
-        async def save(self, session: Session) -> None:
-            pass
-
-        async def get_one(self, _):
-            return session
-
-    watchables_store = InMemoryWatchablesStore()
-
-    session_handler_mocked = SessionHandlerDependency(watchables_store, Mock())
-
-    app.dependency_overrides[session_handler_dependency] = session_handler_mocked
-
-    response = client.post('/session/yo-sí-existo/user/yo-también/vote', json={'watchable_index': 99, 'content': "true"})
+def test_voting_non_existing_watchable(existing_session_and_user):
+    response = client.post(f'/session/{existing_session_and_user[0].id}/user/{existing_session_and_user[1].id}/vote', json={'watchable_index': 99, 'content': "true"})
 
     assert response.status_code == 400
 
     assert response.json()['detail'] == 'The watchable index 99 is out of bounds'
 
-    app.dependency_overrides = {}
 
+def test_voting(existing_session_and_user):
+    session = existing_session_and_user[0]
+    user = existing_session_and_user[1]
 
-def test_voting():
-    watchable = Watchable("Narcos", "Se centra en la historia real de una peligrosa difusión y propagación de una red de cocaína por todo el mundo durante los años 70 y 80.", 2015, WatchableType.SERIES)
-    session = Session('yo-sí-existo', [watchable])
-    session.add_user(User('yo-también'))
-
-    class Mock(SessionStore):
-        async def save(self, session: Session) -> None:
-            pass
-
-        async def get_one(self, _):
-            return session
-
-    watchables_store = InMemoryWatchablesStore()
-
-    session_handler_mocked = SessionHandlerDependency(watchables_store, Mock())
-
-    app.dependency_overrides[session_handler_dependency] = session_handler_mocked
-
-    response = client.post('/session/yo-sí-existo/user/yo-también/vote', json={'watchable_index': 0, 'content': "true"})
+    response = client.post(f'/session/{session.id}/user/{user.id}/vote', json={'watchable_index': 0, 'content': "true"})
 
     assert response.status_code == 201
 
     assert session.get_votes_of_watchable(0)[0] == 1 and session.get_votes_of_watchable(0)[1] == 1
-
-    app.dependency_overrides = {}
 
 
 def test_summary_non_existing_session():
@@ -183,24 +154,8 @@ def test_summary_non_existing_session():
     assert response.status_code == 404
 
 
-def test_summary():
-    watchable = Watchable("Narcos", "Se centra en la historia real de una peligrosa difusión y propagación de una red de cocaína por todo el mundo durante los años 70 y 80.", 2015, WatchableType.SERIES)
-    session = Session('yo-sí-existo', [watchable])
-
-    class Mock(SessionStore):
-        async def save(self, session: Session) -> None:
-            pass
-
-        async def get_one(self, _):
-            return session
-
-    watchables_store = InMemoryWatchablesStore()
-
-    session_handler_mocked = SessionHandlerDependency(watchables_store, Mock())
-
-    app.dependency_overrides[session_handler_dependency] = session_handler_mocked
-
-    response = client.get('/session/yo-sí-existo/summary')
+def test_summary(existing_session_and_vote):
+    response = client.get(f'/session/{existing_session_and_vote.id}/summary')
     body = response.json()
 
     assert response.status_code == 200
@@ -208,5 +163,3 @@ def test_summary():
     assert 'users' in body
 
     assert 'votes' in body
-
-    app.dependency_overrides = {}
