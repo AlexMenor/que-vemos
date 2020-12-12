82c731e166ca0f80800864ce0dc7755c60c2d183
Alex Menor <alex4menor@gmail.com>
2020-12-12 12:09:00 +0100

    Render qr when a session is created so others can join easily

--- frontend/src/views/PreSession.vue
+++ frontend/src/views/PreSession.vue
@@ -1,11 +1,14 @@
 <template>
   <v-layout justify-center align-center style="min-height:100%">
     <card class="pa-8">
-      <v-img src="@/assets/logo-lemur.png"></v-img>
-      <v-btn class="ma-5" color="primary" @click="createSession">
-        Crea una sesión</v-btn
-      >
-      <v-btn class="ma-5"> O únete a una</v-btn>
+      <div v-if="!qr" class="presession-card-content">
+        <v-img src="@/assets/logo-lemur.png"></v-img>
+        <v-btn class="ma-5" color="primary" @click="createSession">
+          Crea una sesión</v-btn
+        >
+        <v-btn class="ma-5"> O únete a una</v-btn>
+      </div>
+      <div id="qr-parent"></div>
     </card>
   </v-layout>
 </template>
@@ -13,6 +16,7 @@
 <script>
 import Card from "@/components/Card.vue";
 import axios from "axios";
+import * as QRCode from "qrcode";
 
 const API_ENDPOINT = "https://que-vemos.herokuapp.com";
 export default {
@@ -20,6 +24,7 @@ export default {
   data() {
     return {
       sessionId: null,
+      qr: null,
     };
   },
   methods: {
@@ -27,9 +32,35 @@ export default {
       const { data } = await axios.post(`${API_ENDPOINT}/session`);
 
       this.sessionId = data["session_id"];
+
+      this.qr = await this.generateQr(this.sessionId);
+    },
+    generateQr(content) {
+      return new Promise((resolve, reject) => {
+        QRCode.toCanvas(
+          content,
+          { errorCorrectionLevel: "H" },
+          (err, canvas) => {
+            if (err) reject(err);
+            resolve(canvas);
+          }
+        );
+      });
+    },
+  },
+  watch: {
+    qr() {
+      const parent = document.getElementById("qr-parent");
+      parent.appendChild(this.qr);
     },
   },
 };
 </script>
 
-<style></style>
+<style>
+.presession-card-content {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+</style>
