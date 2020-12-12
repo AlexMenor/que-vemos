cd8486cd477557e8baba644283e2452c599a4be7
Alex Menor <alex4menor@gmail.com>
2020-12-12 11:35:34 +0100

    Añade CORS Middleware

--- app/app.py
+++ app/app.py
@@ -3,9 +3,18 @@
 from fastapi import FastAPI
 from .middleware.logging import LoggingMiddleware
 from .routes import session_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.add_middleware(LoggingMiddleware)
 
 app.include_router(session_routes.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
