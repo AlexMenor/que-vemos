ca9c3e803ced5a6c903d29f881033655cbd1f9d8
Alex Menor <alex4menor@gmail.com>
2020-12-25 14:40:41 +0100

    Implementa get_one_and_lock para solucionar las condiciones de carrera. Closes #73

--- app/data/session_store/redis_session_store.py
+++ app/data/session_store/redis_session_store.py
@@ -2,9 +2,14 @@
 
 import aioredis
 import pickle
+import asyncio
 from app.entities.session import Session
 from .session_store import SessionStore, SessionNotFound
 from ...config.config import REDIS_URL, config, REDIS_EXPIRATION
+
+
+def get_lock_name(session_id: str):
+    return f'lock-{session_id}'
 
 
 class RedisSessionStore(SessionStore):
@@ -15,6 +20,7 @@ class RedisSessionStore(SessionStore):
     async def save(self, session: Session) -> None:
         pickled_object = pickle.dumps(session)
         await self.__redis.set(session.id, pickled_object, expire=int(config[REDIS_EXPIRATION]))
+        await self.__redis.delete(get_lock_name(session.id))
 
     async def get_one(self, session_id: str) -> Session:
         result = await self.__redis.get(session_id)
@@ -24,6 +30,15 @@ class RedisSessionStore(SessionStore):
         return pickle.loads(result)
 
     async def get_one_and_lock(self, session_id: str) -> Session:
+        acquired = 0
+
+        while acquired == 0:
+            acquired = await self.__redis.setnx(get_lock_name(session_id), 'true')
+            if acquired == 0:
+                await asyncio.sleep(0)
+
+        await self.__redis.pexpire(get_lock_name(session_id), 100)
+
         return await self.get_one(session_id)
 
 
